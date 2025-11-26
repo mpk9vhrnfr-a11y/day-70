088af9123d828fcafd7f690c692742d070855a35
sanaa <Sanaakumar07@gmail.com>
2025-11-26 17:35:36 +0530

    c

--- day 70/main.c
+++ day 70/main.c
@@ -1,15 +1,24 @@
-//
-//  main.c
-//  day 70
-//
-//  Created by Sanaa Kumar on 26/11/25.
-//
-
-#include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return EXIT_SUCCESS;
+int main(void) {
+    char str[200];
+    int i;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if(i == 0 && str[i] >= 'a' && str[i] <= 'z')
+            str[i] = str[i] - 32;
+        else if(str[i-1] == '.' && str[i] == ' ')
+            continue;
+        else if(str[i-1] == '.' && str[i] >= 'a' && str[i] <= 'z')
+            str[i] = str[i] - 32;
+        else if(str[i] >= 'A' && str[i] <= 'Z' && i != 0 && str[i-1] != '.')
+            str[i] = str[i] + 32;
+    }
+
+    printf("%s\n", str);
+    return 0;
 }
+
